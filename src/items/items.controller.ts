630a1dc7b488f49176826b51709fb4492316fe52
AnshulAnand <anandanshul001@gmail.com>
2023-03-09 16:29:22 +0530

    Database set-up

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -8,17 +8,20 @@ import {
   Param,
 } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
 
 @Controller('items')
 export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
   @Get()
-  findAll(): string {
-    return 'Get all items';
+  findAll(): Item[] {
+    return this.itemsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param() param: { id: number }): string {
-    return `Item ${param.id}`;
+  findOne(@Param('id') id): Item {
+    return this.findOne(id);
   }
 
   @Post()
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './interfaces/item.interface';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  private readonly items: Item[] = [
+    {
+      id: '6hft9b3765dfw6d1',
+      name: 'Item 1',
+      qty: 1,
+      description: 'description for item 1',
+    },
+    {
+      id: '6hft9b3765dfw6d2',
+      name: 'Item 2',
+      qty: 2,
+      description: 'description for item 2',
+    },
+    {
+      id: '6hft9b3765dfw6d3',
+      name: 'Item 3',
+      qty: 3,
+      description: 'description for item 3',
+    },
+  ];
+
+  findAll(): Item[] {
+    return this.items;
+  }
+
+  findOne(id: string): Item {
+    return this.items.find((item) => item.id == id);
+  }
+}
